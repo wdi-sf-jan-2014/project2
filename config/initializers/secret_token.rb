d33685a439a71f23090e8e04513acc36858d7193
cbestiva <cbestiva@gmail.com>
2014-02-21 13:40:22 -0800

    first commit & added site controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DatePlannerApp::Application.config.secret_key_base = '1a2f7dce4a119dd57188ac4a3def2e533c84bc7cbf68e44f7eeb0dbd0daac88130f72add0efbd3e39f8fdf06ffd117d5e344ffa6e064933054dc0af97d2ccfcc'
