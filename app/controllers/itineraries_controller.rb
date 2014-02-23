d736fe0093a0c2ae25e101569b82f8121ea060c6
cbestiva <cbestiva@gmail.com>
2014-02-23 14:04:08 -0800

    first commit-basic scaffolding for main page

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,2 +1,15 @@
 class ItinerariesController < ApplicationController
+
+  def index
+    @itineraries = Itinerary.all
+  end
+
+  def new
+    @itinerary = Itinerary.new
+  end
+
+  def create
+    new_itinerary = params.require(:itinerary).permit(:funcheaps, :restaurant)
+    @itinerary = Itinerary.create(new_itinerary)
+  end
 end
