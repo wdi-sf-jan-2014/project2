13bf47a18bb9ca58d3898aecca95cdad60e733e4
David Simons <dave.c.simons@gmail.com>
2014-02-25 11:46:35 -0800

    Functioning activity selector value display

--- app/assets/javascripts/restaurant.js
+++ app/assets/javascripts/restaurant.js
@@ -2,14 +2,17 @@ $(function() {
 
   $("ul#select_source a").click(function(eventObject) {
     eventObject.preventDefault();
-    if ($(this).text() === "Events") {
-      console.log("events, mo-fo!");
+    if ($(this).text() === "Fun & Cheap") {
+      console.log("fun & cheap, mo-fo!");
+      $("input#activity_selector").val("Fun & Cheap");
     }
     else if ($(this).text() === "Restaurants") {
       console.log("restaurants, mo-fo!");
+      $("input#activity_selector").val("Restaurants");
     }
     else if ($(this).text() === "Concerts") {
       console.log("concerts, mo-fo!");
+      $("input#activity_selector").val("Concerts");
     }
     else {
       console.log("No clue, mo-fo!");
