2abc3b0816a30548dc5ecefab8c79d951c1c209f
cbestiva <cbestiva@gmail.com>
2014-02-25 10:46:17 -0800

    generated devise views

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,33 @@ DatePlannerApp::Application.routes.draw do
 
   resources :itineraries 
 
-#           root GET    /                               sites#index
-#        wingman GET    /wingman(.:format)              sites#index
-#    itineraries GET    /itineraries(.:format)          itineraries#index
-#                POST   /itineraries(.:format)          itineraries#create
-#  new_itinerary GET    /itineraries/new(.:format)      itineraries#new
-# edit_itinerary GET    /itineraries/:id/edit(.:format) itineraries#edit
-#      itinerary GET    /itineraries/:id(.:format)      itineraries#show
-#                PATCH  /itineraries/:id(.:format)      itineraries#update
-#                PUT    /itineraries/:id(.:format)      itineraries#update
-#                DELETE /itineraries/:id(.:format)      itineraries#destroy
-
+  # get    '/users/sign_in',        to: 'devise/sessions#new', as: :new_user_session
+  # post   '/users/sign_in',        to: 'devise/sessions#create', as: :user_session
+  # delete '/users/sign_out',       to: 'devise/sessions#destroy', as: :destroy_user_session
+  # post   '/users/password',       to: 'devise/passwords#create', as: :user_password
+  # get    '/users/password/new',   to: 'devise/passwords#new', as: :new_user_password
+  # get    '/users/password/edit',  to: 'devise/passwords#edit', as: :edit_user_password
+  # patch  '/users/password',       to: 'devise/passwords#update'
+  # put    '/users/password',       to: 'devise/passwords#update'
+  # get    '/users/cancel',         to: 'devise/registrations#cancel', as: :cancel_user_registration
+  # post   '/users',                to: 'devise/registrations#create', as: :user_registration
+  # get    '/users/sign_up',        to: 'devise/registrations#new', as: :new_user_registration
+  # get    '/users/edit',           to: 'devise/registrations#edit', as: :edit_user_registration
+  # patch  '/users',                to: 'devise/registrations#update'
+  # put    '/users',                to: 'devise/registrations#update'
+  # delete '/users',                to: 'devise/registrations#destroy'
+#                     root GET    /                               sites#index
+#                  wingman GET    /wingman(.:format)              sites#index
+#                yelpfinds POST   /yelpfinds(.:format)            yelpfinds#create
+#                funcheaps GET    /funcheaps(.:format)            funcheaps#perform_scrape
+#                          POST   /funcheaps(.:format)            funcheaps#create
+#              itineraries GET    /itineraries(.:format)          itineraries#index
+#                          POST   /itineraries(.:format)          itineraries#create
+#            new_itinerary GET    /itineraries/new(.:format)      itineraries#new
+#           edit_itinerary GET    /itineraries/:id/edit(.:format) itineraries#edit
+#                itinerary GET    /itineraries/:id(.:format)      itineraries#show
+#                          PATCH  /itineraries/:id(.:format)      itineraries#update
+#                          PUT    /itineraries/:id(.:format)      itineraries#update
+#                          DELETE /itineraries/:id(.:format)      itineraries#destroy
   
 end
