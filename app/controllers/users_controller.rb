b8ee3d88bae3856b75f78550052cb32ec06317ea
cbestiva <cbestiva@gmail.com>
2014-02-27 15:59:13 -0800

    fixed general styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # binding.pry
+
     @events = current_user.itineraries.sort{|event1, event2| event1[:date] <=> event2[:date]}
   end
 
